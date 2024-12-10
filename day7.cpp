41032bc4494caeb6817e5598d544d2c2f4c56f30
Chitresh Gurjar <chitresh.gurjar8779@gmail.com>
2024-12-10 20:50:15 +0530

    day 7 updated

--- day7.cpp
+++ day7.cpp
@@ -1,3 +1,30 @@
+// A mountain shepherd, wants to send special winter greetings to his fellow shepherds in nearby villages. However, his list of greetings is all combined into a single string using various symbols as separators, such as '.' , '@', '!' etc. Your task is to help him separate these strings into individual greetings so he can send them out clearly.
+
+// Input Format
+
+// The first line will be T, the number of test cases.
+// Each test case will provide a single string containing greetings combined with special characters.
+// Constraints
+
+// 1≤ T≤ 500
+// 1 ≤ S.size() ≤ 1000
+// S contains alphanumeric characters and special symbols: {~,!,@,#,$,%,^,&,*,(,),_,+}
+// Output Format
+
+// For each test case, output the list of separated strings containing greetings.
+
+// Sample Input 0
+
+// 1
+// warm@winter!greetings_to_you
+// Sample Output 0
+
+// warm
+// winter
+// greetings
+// to
+// you
+
 #include <iostream>
 #include <string>
 #include <vector>
