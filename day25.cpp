52ea6fe0c89bf62e72a6028e6f5fc84c5200f9d2
Chitresh Gurjar <chitresh.gurjar8779@gmail.com>
2024-12-28 22:28:28 +0530

    day25

---
+++ day25.cpp
@@ -0,0 +1,44 @@
+/*Sarthak and Samarth, while traversing a mystical desert, discover ancient scrolls hidden in mysterious pyramids. Each scroll contains a square matrix with secret symbols. To unlock the treasure hidden inside the pyramid, the scroll must satisfy two magical conditions:
+
+All diagonal elements of the matrix must be non-zero.
+All other elements of the matrix must be zero.
+Your task is to design a program to determine whether the scroll unlocks the treasure based on the conditions above.
+
+Input Format
+
+The first line contains an integer T, the number of test cases.
+
+For each test case:
+
+The first line contains two integers m and n, representing the dimensions of the matrix.
+The next m lines contain n integers each, representing the elements of the matrix.
+Constraints
+
+1<=T<=100
+3<=m,n<=100
+-100000<=matrix[i][j]<=100000
+Output Format
+
+"true" if the scroll satisfies the magical conditions to unlock the treasure.
+"false" otherwise.
+Sample Input 0
+
+1 
+3 3 
+5 7 0 
+0 3 1 
+0 5 0
+Sample Output 0
+
+false
+Sample Input 1
+
+1
+4 4
+2 0 0 1
+0 3 1 0
+0 5 2 0
+4 0 0 2
+Sample Output 1
+
+true*/
